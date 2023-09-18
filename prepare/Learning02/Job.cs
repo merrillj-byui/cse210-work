41456436919962d442a3c59c4b96101992d59bc9
merrillj-byui <mer23010@byui.edu>
2023-09-17 18:31:41 -0600

    cse210 w02 Learning Activity completed

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,16 @@
+using System;
+
+// Class to abstract a person's job
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
