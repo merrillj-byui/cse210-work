d9618352ce6be9c3f75d3939889e7329c32a7027
merrillj-byui <mer23010@byui.edu>
2023-09-27 20:02:40 -0600

    Learning 03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fract1 = new Fraction();
+        Console.WriteLine(fract1.GetFractionString());
+        Console.WriteLine(fract1.GetDecimalValue());
+
+        Fraction fract2 = new Fraction(5);
+        Console.WriteLine(fract2.GetFractionString());
+        Console.WriteLine(fract2.GetDecimalValue());
+
+        Fraction fract3 = new Fraction(3, 4);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        fract3.SetTop(5);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        fract3.SetBottom(2);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        fract3.SetTop(1);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
+
+        fract3.SetBottom(3);
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue());
     }
 }
